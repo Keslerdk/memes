95023ddd56c0822a9947e779ebb2745724697b46
Daria <kd7614@yandex.ru>
2021-01-28 18:33:28 +0300

    email firsr auth

--- app/src/main/java/com/example/memes/MainActivity.java
+++ app/src/main/java/com/example/memes/MainActivity.java
@@ -1,14 +1,79 @@
 package com.example.memes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    private EditText Elogin;
+    private EditText Epassword;
+    private Button SignIn;
+    private Button SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        Elogin = (EditText) findViewById(R.id.editLogin);
+        Epassword= (EditText) findViewById(R.id.editPassword);
+
+        SignIn= (Button) findViewById(R.id.signIn);
+        SignUp = (Button) findViewById(R.id.signUp);
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signing(Elogin.getText().toString(), Epassword.getText().toString());
+            }
+        });
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registration(Elogin.getText().toString(), Epassword.getText().toString());
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser!=null) {}
+        else {}
+    }
+
+    public void signing (String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) Toast.makeText(MainActivity.this, "Авторизация успешна", Toast.LENGTH_SHORT).show();
+                else Toast.makeText(MainActivity.this, "Авторизация провалена", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void registration (String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) Toast.makeText(MainActivity.this, "Регистрация успешна", Toast.LENGTH_SHORT).show();
+                else Toast.makeText(MainActivity.this, "Регистрация провалена", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
